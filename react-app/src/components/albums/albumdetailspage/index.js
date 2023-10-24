15346c2db1e50a1e601f528ef055094e1b43e28a
Ryan Fournier <ryanfour1637@gmail.com>
2023-10-24 16:50:33 -0700

    working on display albums

--- react-app/src/components/albums/albumdetailspage/index.js
+++ react-app/src/components/albums/albumdetailspage/index.js
@@ -2,73 +2,82 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { getEveryPostThunk } from "../../../store/posts";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useModal } from "../../../context/Modal";
 import OpenModalButton from "../../OpenModalButton";
 import CreateAlbumModel from "../createnewalbummodel";
 
-
 export default function AlbumDetailsPage() {
-    const dispatch = useDispatch()
-    const { setModalContent, setOnModalClose } = useModal();
-    const [refresh, setRefresh] = useState("")
+   const dispatch = useDispatch();
+   const [refresh, setRefresh] = useState("");
+   const [photo, setPhoto] = useState(
+      "https://www.seekpng.com/png/full/346-3460840_add-camera-icon-icon-add.png"
+   );
+   const allPosts = useSelector((state) => state.posts.allPosts);
+   const user = useSelector((state) => state.session.user);
 
-    const allPosts = useSelector((state) => state.posts.allPosts)
-    
-    const user = useSelector((state) => state.session.user)
-    let userId
+   const arrAllPosts = Object.values(allPosts);
 
-    if (user) {
-        userId = user.id
-    }
-   
-    const albumArray = []
-    console.log("🚀 ~ file: index.js:27 ~ AlbumDetailsPage ~ user.albums:", user.albums)
-    user.albums.forEach((album) => albumArray.push(album))
-    console.log("🚀 ~ file: index.js:26 ~ AlbumDetailsPage ~ albumArray:", albumArray)
+   let userId;
 
-    const usersAlbumArray = []
+   if (user) {
+      userId = user.id;
+   }
 
-    // while (albumArray.length) {
-    //    const filteredPosts = allPosts.filter((post) => albumArray[0].id == post.albumId)
-    //    if (filteredPosts)
-    // }
+   const albumArray = [...user.albums[0]];
+   const newAlbumArray = [];
 
+   useEffect(() => {
+      dispatch(getEveryPostThunk());
+   }, [dispatch]);
 
+   if (!arrAllPosts) return null;
+   if (!user) return null;
 
-  
-//     while (setArray.length) {
-//         userPostsFiltered.forEach((post) => {
-//             if (setArray[0] == post.albumId)
-//             usersAlbumArray.push(post)
-//         setArray.shift()
-//     })
-// }
+   while (albumArray.length) {
+      const filteredPosts = arrAllPosts.filter(
+         (post) => albumArray[0].id == post.albumId
+      );
+      if (filteredPosts.length > 0) {
+         albumArray[0]["imageUrl"] = filteredPosts[0].imageUrl;
+         newAlbumArray.shift(albumArray);
+      } else {
+         albumArray[0]["imageUrl"] =
+            "https://www.seekpng.com/png/full/346-3460840_add-camera-icon-icon-add.png";
+         newAlbumArray.shift(albumArray);
+      }
+   }
 
-    
-    useEffect(() => {
-        dispatch(getEveryPostThunk())
-    }, [dispatch])
-
-if (!allPosts) return null
-if (!user) return null
-    return (<div>
-        <h1>Album Details Page</h1>
-        <div>
-                  <OpenModalButton
-                     buttonText="Create New Album"
-                     modalComponent={<CreateAlbumModel refresh={refresh} setRefresh={setRefresh}/>}
-                  />
+   console.log("this is the album array", newAlbumArray);
+   return (
+      <div>
+         <h1>Album Details Page</h1>
+         <div>
+            <OpenModalButton
+               buttonText="Create New Album"
+               modalComponent={
+                  <CreateAlbumModel refresh={refresh} setRefresh={setRefresh} />
+               }
+            />
+         </div>
+         <div className="albumMapping">
+            {newAlbumArray.map((album) => (
+               <div key={album.id}>
+                  <NavLink to="posts/current">
+                     <div className="albumImageTitleContainer">
+                        <div>
+                           <img
+                              className="testIMG"
+                              alt=""
+                              src="https://www.seekpng.com/png/full/346-3460840_add-camera-icon-icon-add.png"
+                           ></img>
+                        </div>
+                        <div>{album.title}</div>
+                     </div>
+                  </NavLink>
                </div>
-        <div className="albumMapping"> {albumArray.map((album) => (
-            <div>
-            <Link to='posts/current'>
-            <div className="albumImageTitleContainer">
-            <div><img className="testIMG" key={album.id} src="https://www.seekpng.com/png/full/346-3460840_add-camera-icon-icon-add.png"></img></div>
-            <div>{album.title}</div>
-            </div>
-            </Link>
-            </div>
-        ))}</div>
-    </div>)
+            ))}
+         </div>
+      </div>
+   );
 }
