6d1a7b3c8221a26622d11ae68ea3a8f6db78390e
Lan <nolan.oribello@gmail.com>
2023-10-24 15:36:14 -0700

    started lan.css and added bg and profile default pics

--- react-app/src/components/UserBanner/index.js
+++ react-app/src/components/UserBanner/index.js
@@ -1,26 +1,26 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {useParams, useHistory} from "react-router-dom";
+import "../CSS/lan.css"
 
 export default function UserBanner() {
   const {id} = useParams();
-  const {push} = useHistory();
-  const dispatch = useDispatch();
   const user = useSelector((state) => state.session.user);
   const post = useSelector((state) => state.posts.allPosts[id]);
   const all_posts = useSelector((state) => state.posts.allPosts);
   const allUserPostsTotal = Object.values(all_posts).filter((singlepost) => singlepost.ownerId === user.id).length
-  console.log("🚀 ~ file: index.js:13 ~ UserBanner ~ allUserPostsTotal:", allUserPostsTotal)
-
 
   return (
-    <div className="you-parent-container">
-      <div className="you-banner-container">
-        <div className="you-user-details">
-          <p>
-            {user.firstName} {user.lastName}
-          </p>
-          <p>{user.username}</p>
+    <div className="banner-parent-container">
+      <div className="banner-banner-container">
+        <div className="banner-user-details">
+            <div id="default-profile-photo">
+                <i className="fa-solid fa-camera"></i>
+            </div>
+            <div>
+                <p>{user.firstName} {user.lastName}</p>
+                <p>{user.username}</p>
+            </div>
         </div>
         <div className="you-photos-joined-container">
           <p>{allUserPostsTotal} Photos</p>
