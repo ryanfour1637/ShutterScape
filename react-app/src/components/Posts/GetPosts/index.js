87b9e700ae18b67a531438390bcbc9e57bb67d1b
Ryan Fournier <ryanfour1637@gmail.com>
2023-10-27 10:17:51 -0700

    fixed double render of photos and added random refresh of photos

--- react-app/src/components/Posts/GetPosts/index.js
+++ react-app/src/components/Posts/GetPosts/index.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { getNineRandomNonOwnerPosts } from "../../../store/posts";
-import "../../CSS/john.css"
+import "../../CSS/john.css";
 
 export default function GetPosts() {
    const { push } = useHistory();
-   const user = useSelector((state) => state.session.user);
    const dispatch = useDispatch();
    const getPosts = useSelector((state) => state.posts.ninePosts);
    const arrPosts = Object.values(getPosts);
-   const posts = [...arrPosts]
+   const [postsToDisplay, setPostsToDisplay] = useState([]);
+   const posts = [...arrPosts];
 
    useEffect(() => {
       dispatch(getNineRandomNonOwnerPosts());
+      setPostsToDisplay(posts);
    }, [dispatch]);
 
    const goToPost = (post) => {
@@ -21,15 +22,25 @@ export default function GetPosts() {
       return;
    };
 
+   const getRandomPhotos = () => {
+      dispatch(getNineRandomNonOwnerPosts());
+      setPostsToDisplay(posts);
+   };
+
    if (Object.values(posts).length === 0) return null;
 
    return (
       <>
          <h1 className="album-page-h1">Trending</h1>
+         <button onClick={getRandomPhotos}>See more photos</button>
          <div className="allposts-photos">
-            {arrPosts.map((post) => (
+            {postsToDisplay.map((post) => (
                <div onClick={() => goToPost(post)} key={post.id}>
-                  <img src={post.photoUrl} alt="" className="userpost-images"></img>
+                  <img
+                     src={post.photoUrl}
+                     alt=""
+                     className="userpost-images"
+                  ></img>
                </div>
             ))}
          </div>
--- react-app/src/store/posts.js
+++ react-app/src/store/posts.js
@@ -3,165 +3,169 @@
 const GET_ALL_POSTS = "/get_all_posts";
 const GET_POST_DETAILS = "/post_details";
 const DELETE_POST = "/delete_post";
-const GET_NINE_POSTS = "/get_nine_posts"
+const GET_NINE_POSTS = "/get_nine_posts";
 
 //action creator
 
-const actionGetPosts = (posts) => ({type: GET_ALL_POSTS, posts});
-const actionGetPostDetails = (post) => ({type: GET_POST_DETAILS, post});
-const actionDeletePost = (id) => ({type: DELETE_POST, id});
-const actionGetNinePosts = (posts) => ({type: GET_NINE_POSTS, posts});
+const actionGetPosts = (posts) => ({ type: GET_ALL_POSTS, posts });
+const actionGetPostDetails = (post) => ({ type: GET_POST_DETAILS, post });
+const actionDeletePost = (id) => ({ type: DELETE_POST, id });
+const actionGetNinePosts = (posts) => ({ type: GET_NINE_POSTS, posts });
 
 //Thunks
 
 //getAllPostsThunk
 export const getTenRandomPostsThunk = () => async (dispatch) => {
-  const res = await fetch("/api/posts");
+   const res = await fetch("/api/posts");
 
-  if (res.ok) {
-    const data = await res.json();
-    dispatch(actionGetNinePosts(data))
-    return data;
-  } else {
-    const errors = await res.json();
-    return errors;
-  }
+   if (res.ok) {
+      const data = await res.json();
+      dispatch(actionGetNinePosts(data));
+      return data;
+   } else {
+      const errors = await res.json();
+      return errors;
+   }
 };
 
 //getAllOwnerPosts Thunk
 export const getNineRandomNonOwnerPosts = () => async (dispatch) => {
-  const res = await fetch("/api/posts/current");
-  if (res.ok) {
-    const data = await res.json();
-    dispatch(actionGetNinePosts(data))
-    return data;
-  } else {
-    const errors = await res.json();
-    return errors;
-  }
+   const res = await fetch("/api/posts/current");
+   if (res.ok) {
+      const data = await res.json();
+      dispatch(actionGetNinePosts(data));
+      return data;
+   } else {
+      const errors = await res.json();
+      return errors;
+   }
 };
 
 export const getEveryPostThunk = () => async (dispatch) => {
-  const res = await fetch("/api/posts/all");
-  if (res.ok) {
-    const data = await res.json();
-    dispatch(actionGetPosts(data));
-    return data;
-  } else {
-    const errors = await res.json();
-    return errors;
-  }
+   const res = await fetch("/api/posts/all");
+   if (res.ok) {
+      const data = await res.json();
+      dispatch(actionGetPosts(data));
+      return data;
+   } else {
+      const errors = await res.json();
+      return errors;
+   }
 };
 
 //getPostDetails Thunk
 export const getPostDetailsThunk = (id) => async (dispatch) => {
-  const res = await fetch(`/api/posts/${id}`);
-  if (res.ok) {
-    const data = await res.json();
-    dispatch(actionGetPostDetails(data));
-    return data;
-  } else {
-    const errors = await res.json();
-    return errors;
-  }
+   const res = await fetch(`/api/posts/${id}`);
+   if (res.ok) {
+      const data = await res.json();
+      dispatch(actionGetPostDetails(data));
+      return data;
+   } else {
+      const errors = await res.json();
+      return errors;
+   }
 };
 
 //createPost Thunk
 export const createPostThunk = (form) => async (dispatch) => {
-  const res = await fetch("/api/posts/new", {
-    method: "POST",
-    body: form,
-  });
+   const res = await fetch("/api/posts/new", {
+      method: "POST",
+      body: form,
+   });
 
-  if (res.ok) {
-    const {resPost} = await res.json();
-    dispatch(actionGetPostDetails(resPost));
-    return resPost;
-  } else {
-    const data = await res.json();
-    return data;
-  }
+   if (res.ok) {
+      const { resPost } = await res.json();
+      dispatch(actionGetPostDetails(resPost));
+      return resPost;
+   } else {
+      const data = await res.json();
+      return data;
+   }
 };
 
 //createPostNoAlbums Thunk
 export const createPostThunkNoAlbums = (form) => async (dispatch) => {
-  const res = await fetch("/api/posts/no_album", {
-    method: "POST",
-    body: form,
-  });
+   const res = await fetch("/api/posts/no_album", {
+      method: "POST",
+      body: form,
+   });
 
-  if (res.ok) {
-    const {resPost} = await res.json();
-    dispatch(actionGetPostDetails(resPost));
-    return resPost;
-  } else {
-    const data = await res.json();
-    return data;
-  }
+   if (res.ok) {
+      const { resPost } = await res.json();
+      dispatch(actionGetPostDetails(resPost));
+      return resPost;
+   } else {
+      const data = await res.json();
+      return data;
+   }
 };
 
 //updatePost Thunk
 export const updatePostThunk = (form, postId) => async (dispatch) => {
-  try {
-    const res = await fetch(`/api/posts/update/${postId}`, {
-      method: "PUT",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(form),
-    });
+   try {
+      const res = await fetch(`/api/posts/update/${postId}`, {
+         method: "PUT",
+         headers: { "Content-Type": "application/json" },
+         body: JSON.stringify(form),
+      });
 
-    if (res.ok) {
-      const resPost = await res.json();
-      return resPost;
-    } else {
-      return {errors: "There was an error updating your post"};
-    }
-  } catch (error) {
-    const data = await JSON.stringify(error);
-    return data;
-  }
+      if (res.ok) {
+         const resPost = await res.json();
+         return resPost;
+      } else {
+         return { errors: "There was an error updating your post" };
+      }
+   } catch (error) {
+      const data = await JSON.stringify(error);
+      return data;
+   }
 };
 
 //deletePost Thunk
 export const deletePostThunk = (id) => async (dispatch) => {
+   const res = await fetch(`/api/posts/delete/${id}`, {
+      method: "DELETE",
+   });
 
-  const res = await fetch(`/api/posts/delete/${id}`, {
-    method: "DELETE",
-  });
-
-  if (res.ok) {
-    dispatch(actionDeletePost(id));
-  } else {
-    return {"errors": "There was an error deleting your post!"};
-  }
+   if (res.ok) {
+      dispatch(actionDeletePost(id));
+   } else {
+      return { errors: "There was an error deleting your post!" };
+   }
 };
 
 //Reducer
 
-const initialState = {allPosts: {}, ninePosts: {}};
+const initialState = { allPosts: {}, ninePosts: {} };
 
 export default function postReducer(state = initialState, action) {
-  let newState;
-  switch (action.type) {
-    case GET_ALL_POSTS:
-      newState = {...state, allPosts: {}};
-      action.posts.forEach((post) => (newState.allPosts[post.id] = post));
-      return newState;
+   let newState;
+   switch (action.type) {
+      case GET_ALL_POSTS:
+         newState = { ...state, allPosts: {} };
+         action.posts.forEach((post) => (newState.allPosts[post.id] = post));
+         return newState;
 
-    case GET_POST_DETAILS:
-      newState = {...state, allPosts: {...state.allPosts}};
-      newState.allPosts[action.post.id] = action.post;
-      return newState;
+      case GET_POST_DETAILS:
+         newState = { ...state, allPosts: { ...state.allPosts } };
+         newState.allPosts[action.post.id] = action.post;
+         return newState;
 
-    case DELETE_POST:
-      newState = {...state, allPosts: {...state.allPosts}};
-      delete newState.allPosts[action.id];
-      return newState;
+      case DELETE_POST:
+         newState = { ...state, allPosts: { ...state.allPosts } };
+         delete newState.allPosts[action.id];
+         return newState;
 
-    case GET_NINE_POSTS:
-      newState = { ...state, ninePosts: {...action.posts}}
-      return newState;
+      case GET_NINE_POSTS:
+         newState = { ...state, ninePosts: {} };
+         action.posts.forEach((post) => (newState.ninePosts[post.id] = post));
+         console.log(
+            "🚀 ~ file: posts.js:163 ~ postReducer ~ newState:",
+            newState
+         );
+         return newState;
 
-    default:
-      return state;
-  }
+      default:
+         return state;
+   }
 }
