39d6d9aed4ef82ef33f54c45bb72fc09659b2bff
Lan <nolan.oribello@gmail.com>
2023-11-16 14:58:21 -0800

    added useEffect to Search and removed comments

--- react-app/src/components/Search/SearchBar.jsx
+++ react-app/src/components/Search/SearchBar.jsx
@@ -28,14 +28,12 @@ const SearchBar = ({ setResults, setIsResultsOpen }) => {
 
   useEffect(() => {
     const handleClickOutside = (event) => {
-      // Check if the click is outside the search input
+
       setSearch("")
     };
 
-    // Add event listener when the component mounts
     document.addEventListener('click', handleClickOutside);
 
-    // Clean up the event listener when the component unmounts
     return () => {
       document.removeEventListener('click', handleClickOutside);
     };
