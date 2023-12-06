3992726712be1b6d6d187e86e8fda338291ae94a
Lan <nolan.oribello@gmail.com>
2023-12-06 12:53:00 -0800

    added image to profile button, styled Navigation component to closely resemble Flickr more

--- react-app/src/components/Search/ResultsList.jsx
+++ react-app/src/components/Search/ResultsList.jsx
@@ -1,23 +1,17 @@
-import React from 'react'
-import SearchResult from './SearchResult'
-import "../Search/SearchBar.css"
+import React from "react";
+import SearchResult from "./SearchResult";
+import "../Search/SearchBar.css";
 
 const ResultsList = ({results, clearSearch}) => {
-
-
-
   return (
-    <div className='results-list'>
-{
-results.map((result, id) => {
-  return <SearchResult result={result} key={id} clearSearch={clearSearch} />;
-})
-
-
-}
-
+    <div className="results-list">
+      {results.map((result, id) => {
+        return (
+          <SearchResult result={result} key={id} clearSearch={clearSearch} />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ResultsList
+export default ResultsList;
